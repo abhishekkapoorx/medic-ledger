31a75f6147ceea9ac0904c838f278c2e94ee38c6
Abhishek Kapoor <kapoorkk1980@gmail.com>
2025-04-10 11:37:42 +0530

    added login page raw

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-24">
-
-      hi
 
     </div>
   );
