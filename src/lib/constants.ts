8e825ef72ba16b0ffb6931f922f12686866b01f0
Abhishek Kapoor <kapoorkk1980@gmail.com>
2025-04-10 21:12:39 +0530

    Dashboard made for user

---
+++ src/lib/constants.ts
@@ -0,0 +1,9 @@
+// { Patient, Doctor, Manufacturer, Distributor, Retailer, Donor }
+export const UserRoles = [
+    "Patient",
+    "Doctor",
+    "Manufacturer",
+    "Distributor",
+    "Retailer",
+    "Donor"
+]
